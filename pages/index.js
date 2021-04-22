9302604d7c74eacae61b42f8ce284f7112f6e25e
Noah Atkins <noah.c.atkins@gmail.com>
2021-04-22 05:59:51 -0400

    css fix

--- pages/index.js
+++ pages/index.js
@@ -41,7 +41,7 @@ export default function Home() {
         <Thumbnail path="/palette/black" title="Black" color="black" />
       </div>
       <div className="homeFooter">
-        <span>© 2021 Noah Atkins </span>
+        <span>© 2021 Hue Hub</span>
       </div>
     </div>
   );
