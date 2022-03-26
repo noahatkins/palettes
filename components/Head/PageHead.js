f7d1fefb4212ca726345ab732cf444e7b5213fdb
Noah Atkins <noah.c.atkins@gmail.com>
2022-03-26 19:14:58 -0400

    meat desc

--- components/Head/PageHead.js
+++ components/Head/PageHead.js
@@ -13,22 +13,22 @@ export default function PageHead({title}) {
         content="atkins,bank,black,blue,color,green,grey,noah,orange,pink,purple,yellow, noah atkins, color bank, color palette, colour bank, hue, hue hub"></meta>
       <meta
         name="description"
-        content="Hue Hub offers 8 color palettes ranging across the color spectrum designed to help you with you find your perfect project colors."></meta>
+        content="Hue Hub offers 8 different color palettes ranging across the color spectrum carefully crafted to help you find your perfect project colors."></meta>
 
       <meta property="og:type" content="website"></meta>
-      <meta property="og:url" content="https://colorbank.noahatkins.com/"></meta>
+      <meta property="og:url" content="https://huehub.noahatkins.com/"></meta>
       <meta property="og:title" content="Hue Hub — Find Your True Colours"></meta>
       <meta
         property="og:description"
-        content="Hue Hub offers 8 color palettes ranging across the color spectrum designed to help you with you find your perfect project colors."></meta>
+        content="Hue Hub offers 8 different color palettes ranging across the color spectrum carefully crafted to help you find your perfect project colors."></meta>
       <meta property="og:image" content="/huehub.png"></meta>
 
       <meta property="twitter:card" content="summary_large_image"></meta>
-      <meta property="twitter:url" content="https://colorbank.noahatkins.com/"></meta>
+      <meta property="twitter:url" content="https://huehub.noahatkins.com/"></meta>
       <meta property="twitter:title" content="Hue Hub — Find Your True Colours"></meta>
       <meta
         property="twitter:description"
-        content="Hue Hub offers 8 color palettes ranging across the color spectrum designed to help you with you find your perfect project colors."></meta>
+        content="Hue Hub offers 8 different color palettes ranging across the color spectrum carefully crafted to help you find your perfect project colors."></meta>
       <meta property="twitter:image" content="/huehub.png"></meta>
     </Head>
   );
